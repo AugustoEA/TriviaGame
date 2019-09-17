1b37c625927ea06628819fa01d34180f473454ee
Augusto Andres <gutoea@gmail.com>
2019-09-16 20:15:18 -0500

    First variables and functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,38 +1,68 @@
+//Array with all the trivia questions.
+
 var quizQuestions = [
     {
         question: 'What is the largest US State?',
         choices: ['Texas','California', 'Puerto Rico', 'Alaska'],
         correctAnswer:'Alaska'
-    }
+    },
 
     {
         question: 'What is the largest country by area in the world?',
         choices: ['US', 'Canada', 'Russia', 'China'],
         correctAnswer:'Russia'
-    }
+    },
 
     {
-        question: 'What is the best selling videogame of all time?'
+        question: 'What is the best selling videogame of all time?',
         choices: ['GTA V', 'Minecraft', 'Tetris', 'Super Mario Bros'],
         correctAnswer:'Minecraft'
-    }
+    },
 
     {
         question: 'Which country won the 2018 World Cup?',
         choices: ['Brazil', 'Germany', 'France', 'Netherlands'],
         correctAnswer:'France'
-    }
+    },
 
     {
         question: 'What is the biggest event that happens in Austin every year?',
         choices: ['UT vs. LSU', 'USGP F1', 'ACL', 'SXSW'],
         correctAnswer:'SXSW'
-    }
+    },
 
     {
         question: "What racing series won't come to Austin in 2019",
         choices: ['MOTOGP', 'F1', 'Indycar', 'WEC'],
         correctAnswer:'WEC'
+    }]
+    // console.log(quizQuestions);
+
+    //Initial Values
+    var counter = 30;
+    var currentQuestion = 0;
+    var score = 0;
+    var lost = 0;
+    var timer;
+
+    //Display questions and options
+    function loadQuestions() {
+        var question = quizQuestions[currentQuestion].question;
+        var choices = quizQuestions[currentQuestion].choices;
+
+        $('#time').html('Time Left: ' + counter);
+        $('#game').html(`
+        <h4>${question}</h4>
+    `);
     }
 
-]
+    function loadChoices(choices) {
+        var result = '';
+        for (var i = 0; i < choices.length; i++) {
+            result += `<p class='choice' data-answer='${choices[i]}'>${choices[i]}</p>`;
+        }
+
+        return result;
+    }
+
+    loadQuestions();
