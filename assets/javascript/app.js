104062955f90e98acaadf8ed883bbf7dcf9b6c88
Augusto Andres <gutoea@gmail.com>
2019-09-16 19:45:36 -0500

    HTML done and trivia questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,38 @@
+var quizQuestions = [
+    {
+        question: 'What is the largest US State?',
+        choices: ['Texas','California', 'Puerto Rico', 'Alaska'],
+        correctAnswer:'Alaska'
+    }
+
+    {
+        question: 'What is the largest country by area in the world?',
+        choices: ['US', 'Canada', 'Russia', 'China'],
+        correctAnswer:'Russia'
+    }
+
+    {
+        question: 'What is the best selling videogame of all time?'
+        choices: ['GTA V', 'Minecraft', 'Tetris', 'Super Mario Bros'],
+        correctAnswer:'Minecraft'
+    }
+
+    {
+        question: 'Which country won the 2018 World Cup?',
+        choices: ['Brazil', 'Germany', 'France', 'Netherlands'],
+        correctAnswer:'France'
+    }
+
+    {
+        question: 'What is the biggest event that happens in Austin every year?',
+        choices: ['UT vs. LSU', 'USGP F1', 'ACL', 'SXSW'],
+        correctAnswer:'SXSW'
+    }
+
+    {
+        question: "What racing series won't come to Austin in 2019",
+        choices: ['MOTOGP', 'F1', 'Indycar', 'WEC'],
+        correctAnswer:'WEC'
+    }
+
+]
